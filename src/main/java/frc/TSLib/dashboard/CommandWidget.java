64691fef4186e208d1ea6e96ae7da70e1ece2945
Team Streak 7067 <frc7067@gmail.com>
2026-01-23 15:44:33 +0200

    update command widgets to use runnables

--- src/main/java/frc/TSLib/dashboard/CommandWidget.java
+++ src/main/java/frc/TSLib/dashboard/CommandWidget.java
@@ -4,6 +4,7 @@ import java.util.function.Supplier;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 
 public class CommandWidget {
@@ -13,6 +14,12 @@ public class CommandWidget {
 			.withName(name).ignoringDisable(ignoreDisable)
 		);
 	}
+
+	public static void addWidget(String name, Runnable command, boolean ignoreDisable) {
+		SmartDashboard.putData(
+			Commands.runOnce(command::run).withName(name).ignoringDisable(ignoreDisable)
+		);
+	}
 	
 	public static void add2StateWidget(String name, Supplier<Command> start, Supplier<Command> end, boolean ignoreDisable) {
 		SmartDashboard.putData(
@@ -20,6 +27,36 @@ public class CommandWidget {
 			new StartEndCommand(
 				()->start.get(),
 				()->end.get()
+			).withName(name).ignoringDisable(ignoreDisable)
+		);
+	}
+
+	public static void add2StateWidget(String name, Runnable start, Supplier<Command> end, boolean ignoreDisable) {
+		SmartDashboard.putData(
+			name,
+			new StartEndCommand(
+				start,
+				()->end.get()
+			).withName(name).ignoringDisable(ignoreDisable)
+		);
+	}
+
+	public static void add2StateWidget(String name, Supplier<Command> start, Runnable end, boolean ignoreDisable) {
+		SmartDashboard.putData(
+			name,
+			new StartEndCommand(
+				()->start.get(),
+				end
+			).withName(name).ignoringDisable(ignoreDisable)
+		);
+	}
+
+	public static void add2StateWidget(String name, Runnable start, Runnable end, boolean ignoreDisable) {
+		SmartDashboard.putData(
+			name,
+			new StartEndCommand(
+				start,
+				end
 			).withName(name).ignoringDisable(ignoreDisable)
 		);
 	}
