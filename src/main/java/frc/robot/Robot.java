63905892b34d9c8122d17e56cda0582966aa7411
Team Streak 7067 <frc7067@gmail.com>
2026-02-15 15:20:04 +0200

    leds

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,15 +7,21 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.TSLib.leds.LedStrip;
 
 public class Robot extends TimedRobot {
 	private Command m_autonomousCommand;
+    LedStrip leds = new LedStrip(0, 9);
 	
 	private final RobotContainer m_robotContainer;
 	
 	public Robot() {
 		m_robotContainer = new RobotContainer();
 	}
+
+    @Override
+    public void robotInit() {
+    }
 	
 	@Override
 	public void robotPeriodic() {
